50220c861fd12308a7f6a577fa509ea10384ea79
Cesar Acosta <c3sar.adan@gmail.com>
2023-03-05 19:03:03 -0700

    SliverAppBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,12 +5,49 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //Recibir argumentos
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'Sin nombre';
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Detalles de la pelicula'),
-      ),
-      body: const Center(
-        child: Text('Details Screen'),
+        body: CustomScrollView(
+      //Widgets con comportamientos predefinidos al hacer scroll
+      slivers: [_CustomAppBar()],
+    ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      //Para que no desaparezca
+      pinned: true,
+      //Widget especial para que se aguste al tamaño
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        //Eliminar padding
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: const Text(
+            'movie.title',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          //Expandir la imagen
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
