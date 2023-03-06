c2a7a85cf1d6dc92afc42a6b60b0a36b5c4bae0f
Cesar Acosta <c3sar.adan@gmail.com>
2023-03-05 17:48:06 -0700

    Swipper - carrusel de tarjetas

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,8 +5,11 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detalles de la pelicula'),
+      ),
+      body: const Center(
         child: Text('Details Screen'),
       ),
     );
